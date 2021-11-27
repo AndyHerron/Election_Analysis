bc33ea684c32315a29a950165948fff26882e863
Andy Herron <coachfozzie@gmail.com>
2021-11-27 00:26:37 -0500

    Adding files

---
+++ Python_practice.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+x = 5+2*3
+print("X =",x) 
+y= 8//5-3
+print("y =",y)
+z=8+22*2-4
+print ("z =",z)
+n = 3**3%5
+print ("n =",n)
+# Dictionary practice
+counties_dict = {}
+counties_dict["Aarapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+print(counties_dict)
+print(counties_dict.keys())
+print(counties_dict.values())
